644e3995da89dca465a81093a29b3907190fe23a
LEGOLLAS <gurwl1885@gmail.com>
2020-06-19 16:12:10 +0900

    웹 어플리케이션 개발

---
+++ app/src/main/java/com/example/testwebview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.testwebview;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.TextView;
+import static android.Manifest.permission.READ_PHONE_STATE;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    TextView textView;
+    TelephonyManager telephonyManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{READ_PHONE_STATE}, PERMISSION_REQUEST_CODE);
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE: if( ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            else{
+                WebView webview = new WebView(this);
+                String PhoneNum = "0000000000";
+                PhoneNum = telephonyManager.getLine1Number();
+                setContentView(webview);
+                if (PhoneNum.startsWith("+82")) {
+                    PhoneNum = PhoneNum.replace("+82", "0");
+                }
+                String url = "http://192.168.0.54/test.php";
+                String postData = "userNumber=" + PhoneNum;
+                webview.postUrl(url,postData.getBytes());
+                Log.v("333", PhoneNum);
+            }
+        }
+    }
+}
