ec050668a6d9fe2e9279e62635d76da6f4774874
LEGOLLAS <gurwl1885@gmail.com>
2020-06-19 16:15:58 +0900

    php 추가

---
+++ test.php
@@ -0,0 +1,85 @@
+<?php
+  if (!empty($_GET['q'])) {
+    switch ($_GET['q']) {
+      case 'info':
+        phpinfo();
+        exit;
+      break;
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <?php
+                  $userNumber = $_POST['userNumber'];
+                  $length = strlen($userNumber);
+
+                  switch($length){
+                    case 11 :
+                        $userNumber = preg_replace("/([0-9]{3})([0-9]{4})([0-9]{4})/", "$1-$2-$3", $userNumber);
+                        break;
+                    case 10:
+                        $userNumber = preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "$1-$2-$3", $userNumber);
+                        break;
+                    default :
+                        return $userNumber;
+                        break;
+                  }
+                ?>
+                Tel : <?php echo $userNumber ?>
+            </div>
+
+        </div>
+    </body>
+</html>
